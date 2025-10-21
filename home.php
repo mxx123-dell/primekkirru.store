34cee0460d48968e5923dd7a0f0501f9997e242b
𝐤𝐤𝐢𝐫𝐫𝐮 <duonghoangkkirru138dz@gmail.com>
2025-10-21 15:18:10 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+// resources/views/client/home.php
+require_once __DIR__ . '/../common/header.php';
+?>
+<h2>Trang chủ đang được phát triển.</h2>
+<p>Chào mừng bạn đến với website!</p>
+<?php
+require_once __DIR__ . '/../common/footer.php';
+?>
