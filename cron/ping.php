b77dfd3ec71d56c874007392a4e1b9ee48026622
𝐤𝐤𝐢𝐫𝐫𝐮 <duonghoangkkirru138dz@gmail.com>
2025-10-21 13:39:18 +0700

    Delete cron directory

--- cron/ping.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Ping giữ cho host không ngủ - by PrimeKkirru.Store
-header("Content-Type: text/plain");
-
-echo "PING OK - " . date("H:i:s d/m/Y");
-
-// Nếu muốn kiểm tra log ping hoạt động thì bật đoạn này:
-// $logFile = __DIR__ . "/ping_log.txt";
-// file_put_contents($logFile, "Ping at " . date("H:i:s d/m/Y") . "\n", FILE_APPEND);
